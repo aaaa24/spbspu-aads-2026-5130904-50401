3fa075d5d28629a61fb5687a5a8a470d86f293e6
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-07 18:33:22 +0300

    done LIter

--- chernov.arseniy/S1/liter.hpp
+++ chernov.arseniy/S1/liter.hpp
@@ -1,10 +1,22 @@
 #ifndef LITER_HPP
 #define LITER_HPP
 
+#include "list.hpp"
+
 namespace chernov {
   template< class T >
   class LIter {
     friend class List< T >;
+    Node< T > * ptr;
+  public:
+    LIter();
+    LIter(Node< T > * node);
+    bool hasNext() const noexcept;
+    LIter< T > next() const;
+    T & value() const;
+    T & operator*() const;
+    bool operator==(const LIter< T > & other) const noexcept;
+    bool operator!=(const LIter< T > & other) const noexcept;
   };
 }
 
